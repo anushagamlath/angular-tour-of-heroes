c4d907dfb687d7dc48fc19d1f832ea3e6cdf6771
Anusha Gamlath <anugamlath@gmail.com>
2024-05-20 11:58:37 +0200

    hero service and message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import {HEROES} from '../mock-heroes';
 import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   standalone: true,
@@ -14,11 +18,25 @@ import {HeroDetailComponent} from '../hero-detail/hero-detail.component';
       <input [(ngModel)]="hero.name">
   `,
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
